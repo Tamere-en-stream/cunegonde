70c4a0e6cf26b5cd6fdcd734232a760a922092e0
Fantome Beignet <himself@fantomebeig.net>
2023-03-12 19:02:28 +0100

    chore: setup skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.postcss';
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-vintage.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+</script>
+
+<slot />
